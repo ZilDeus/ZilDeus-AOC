7e04e57346fbb270d6799df79262686fb414d0fb
zildeus <zildeus@protonmail.com>
2024-12-01 19:44:31 +0300

    day 1 part 2 - completed

--- day-1/main.go
+++ day-1/main.go
@@ -9,13 +9,17 @@ import (
 )
 
 func main() {
+	P1()
+	P2()
+}
+func P1() {
 
 	var list1 []int
 	var list2 []int
 
-	dat, _ := os.ReadFile("test.txt")
+	data, _ := os.ReadFile("test.txt")
 
-	lines := strings.Split(string(dat), "\n")
+	lines := strings.Split(string(data), "\n")
 	lines_count := len(lines) - 1
 
 	for i := 0; i < lines_count; i++ {
@@ -33,6 +37,46 @@ func main() {
 
 	for i := 0; i < len(list1); i++ {
 		result += int(math.Abs(float64(list1[i] - list2[i])))
+	}
+
+	fmt.Println(result)
+}
+
+func P2() {
+
+	var list1 []int
+	var list2 []int
+
+	ls_map := make(map[int]int)
+
+	data, _ := os.ReadFile("test.txt")
+
+	lines := strings.Split(string(data), "\n")
+	lines_count := len(lines) - 1
+
+	for i := 0; i < lines_count; i++ {
+		var n1 int
+		var n2 int
+		fmt.Sscanf(lines[i], "%d   %d", &n1, &n2)
+		list1 = append(list1, n1)
+		list2 = append(list2, n2)
+	}
+
+	for i := 0; i < lines_count; i++ {
+		ls_map[list1[i]] = 0
+	}
+
+	for i := 0; i < lines_count; i++ {
+		_, exists := ls_map[list2[i]]
+		if exists {
+			ls_map[list2[i]]++
+		}
+	}
+
+	result := 0
+
+	for i := 0; i < lines_count; i++ {
+		result += list1[i] * ls_map[list1[i]]
 	}
 
 	fmt.Println(result)
