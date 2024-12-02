e6362461c7bbb6e7b3ccda6e893c91af07f40ba7
zildeus <zildeus@protonmail.com>
2024-12-02 23:14:36 +0300

    day 2 part 2 - completed

--- day-2/main.go
+++ day-2/main.go
@@ -19,6 +19,7 @@ func isStable(n []int) int {
 	}
 	return 1
 }
+
 func isInOrder(n []int) bool {
 	l := len(n)
 	isAsc := func() bool {
@@ -42,10 +43,44 @@ func isInOrder(n []int) bool {
 	return isAsc || isDesc
 }
 
-func main() {
-	P1()
-	//P2()
+func segmentSlice(slice []int) [][]int {
+	var slices [][]int
+	for i := 0; i < len(slice); i++ {
+		var s []int
+		for j, v := range slice {
+			if i != j {
+				s = append(s, v)
+			}
+		}
+		slices = append(slices, s)
+	}
+	return slices
 }
+
+func isStable2(n []int) int {
+	slices := segmentSlice(n)
+	for _, v := range slices {
+		if isStable(v) == 1 {
+			return 1
+		}
+	}
+	return 0
+}
+
+func isInOrder2(slices [][]int) bool {
+	for _, v := range slices {
+		if isInOrder(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	//P1()
+	P2()
+}
+
 func P1() {
 
 	var list [][]int
@@ -76,4 +111,29 @@ func P1() {
 }
 
 func P2() {
+	var list [][]int
+
+	data, _ := os.ReadFile("test.txt")
+
+	lines := strings.Split(string(data), "\n")
+	lines_count := len(lines) - 1
+
+	for i := 0; i < lines_count; i++ {
+		number_string := strings.Split(lines[i], " ")
+		numbers := make([]int, len(number_string))
+		for i, v := range number_string {
+			var n int
+			fmt.Sscanf(v, "%d", &n)
+			numbers[i] = n
+		}
+		list = append(list, numbers)
+	}
+
+	result := 0
+
+	for i := 0; i < len(list); i++ {
+		result += isStable2(list[i])
+	}
+
+	fmt.Println(result)
 }
