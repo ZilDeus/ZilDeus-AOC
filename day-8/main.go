21d4d11c5a28247af0245f2f35edcb8e9763daf7
zildeus <zildeus@protonmail.com>
2024-12-09 21:50:25 +0300

    day 8 part 2 - completed

--- day-8/main.go
+++ day-8/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	P1()
+	//P1()
 	P2()
 }
 
@@ -26,15 +26,22 @@ func generate_signal_map(input []string) [][]rune {
 	return signal_map
 }
 
-func set_antinode(x int, y int, antinodes_map [][]rune) {
-	if y < 0 || y >= len(antinodes_map) {
-		return
+func is_antinode_in_range(x int, y int, _map [][]rune) bool {
+	if y < 0 || y >= len(_map) {
+		return false
 	}
-	if x < 0 || x >= len(antinodes_map[0]) {
-		return
+	if x < 0 || x >= len(_map[0]) {
+		return false
 	}
-	fmt.Println(x, y)
-	antinodes_map[y][x] = '#'
+	return true
+}
+
+func set_antinode(x int, y int, antinodes_map [][]rune) bool {
+	if is_antinode_in_range(x, y, antinodes_map) {
+		antinodes_map[y][x] = '#'
+		return true
+	}
+	return false
 }
 
 func get_antinodes_coords(antenna_coords [][2]int) [][2]int {
@@ -78,6 +85,65 @@ func get_antinodes_map(signal [][]rune) [][]rune {
 	for _, v := range signal_map {
 		coords := get_antinodes_coords(v)
 		for _, c := range coords {
+			if set_antinode(c[0], c[1], antinodes_map) {
+			}
+		}
+	}
+
+	return antinodes_map
+}
+
+func generate_the_fucking_nodes(origin [2]int, point [2]int, coords *[][2]int, signal [][]rune) {
+	if origin[0] == point[0] && origin[1] == point[1] {
+		return
+	}
+	x := point[0] + (point[0] - origin[0])
+	y := point[1] + (point[1] - origin[1])
+	if is_antinode_in_range(x, y, signal) {
+		*coords = append(*coords, [2]int{x, y})
+		generate_the_fucking_nodes(point, [2]int{x, y}, coords, signal)
+	}
+}
+func get_antinodes_map2(signal [][]rune) [][]rune {
+	antinodes_map := make([][]rune, len(signal))
+	for i := range len(signal) {
+		antinodes_map[i] = make([]rune, len(signal[0]))
+		for j := 0; j < len(signal[0]); j++ {
+			antinodes_map[i][j] = '.'
+		}
+	}
+
+	signal_map := make(map[rune][][2]int)
+
+	for y, line := range signal {
+		for x, char := range line {
+			if char != '#' && char != '.' {
+				_, exists := signal_map[char]
+				if !exists {
+					signal_map[char] = make([][2]int, 0)
+				}
+				signal_map[char] = append(signal_map[char], [2]int{x, y})
+			}
+		}
+	}
+
+	for _, v := range signal_map {
+		coords := make([][2]int, 0)
+		for i := range v {
+			for j := range v {
+				if i != j {
+					continue
+				}
+				x := v[i][0] + (v[i][0] - v[j][0])
+				y := v[i][1] + (v[i][1] - v[j][1])
+				if is_antinode_in_range(x, y, signal) {
+					coords = append(coords, [2]int{x, y})
+					generate_the_fucking_nodes(v[i], [2]int{x, y}, &coords, signal)
+					generate_the_fucking_nodes(v[j], [2]int{x, y}, &coords, signal)
+				}
+			}
+		}
+		for _, c := range coords {
 			set_antinode(c[0], c[1], antinodes_map)
 		}
 	}
@@ -89,7 +155,22 @@ func P1() {
 	data, _ := os.ReadFile("test.txt")
 	signal_map := generate_signal_map(strings.Split(string(data), "\n"))
 	antinodes_map := get_antinodes_map(signal_map)
-	fmt.Println(antinodes_map)
+	result := 0
+	for _, line := range antinodes_map {
+		for _, char := range line {
+			if char == '#' {
+				result++
+			}
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func P2() {
+	data, _ := os.ReadFile("test.txt")
+	signal_map := generate_signal_map(strings.Split(string(data), "\n"))
+	antinodes_map := get_antinodes_map2(signal_map)
 	result := 0
 	for _, line := range antinodes_map {
 		fmt.Println(string(line))
@@ -99,11 +180,12 @@ func P1() {
 			}
 		}
 	}
+	for _, line := range signal_map {
+		for _, char := range line {
+			if char != '.' {
+				result++
+			}
+		}
+	}
 	fmt.Println(result)
-}
-
-func P2() {
-	//data, _ := os.ReadFile("test.txt")
-	//strings.Split(string(data), "\n")
-	//fmt.Println(input)
 }
